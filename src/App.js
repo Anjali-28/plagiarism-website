7930e7cdb93ab79df19aa79087c3bd87e4c67e50
arjunsudheer001 <arjunsudheer1@gmail.com>
2023-05-19 16:40:20 +0530

    conflict resolution

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Navbar } from './components';
 
-import { Landing, Login, Signup, Pendingassn, Submitassn } from './pages'
+import { Landing, Login, Signup, Pendingassn, Submitassn, Contact, TeacherLogin, TeacherSignup, StudentProfile } from './pages'
 
 function App() {
   return (
@@ -15,6 +15,10 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/pendingassn" element={<Pendingassn />} />
           <Route path="/submitassn" element={<Submitassn />} />
+          <Route path="/contactus" element={<Contact/>}/>
+          <Route path="/teacherlogin" element={<TeacherLogin/>}/>
+          <Route path="/teachersignup" element={<TeacherSignup/>}/>
+          <Route path="/studentprofile" element={<StudentProfile/>}/>
         </Routes>
       </BrowserRouter>
     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,4 +3,7 @@ export { default as Login } from './Login/Login'
 export { default as Signup } from './Signup/Signup'
 export { default as Pendingassn } from './Pendingassn/Pendingassn'
 export { default as Submitassn } from './Submitassn/Submitassn'
-
+export { default as Contact } from './Contact/Contact'
+export { default as TeacherLogin} from './TeacherLogin/TeacherLogin'
+export { default as TeacherSignup} from './TeacherSignup/TeacherSignup'
+export { default as StudentProfile} from './StudentProfile/StudentProfile'
