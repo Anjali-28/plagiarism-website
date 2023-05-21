bc740889dbee74156310d33b92236112f11389b8
Anjali Rajendran <73744625+Anjali-28@users.noreply.github.com>
2023-05-21 11:50:03 +0530

    latest

--- src/components/Subcomplete_comp/Subcomplete_comp.js
+++ src/components/Subcomplete_comp/Subcomplete_comp.js
@@ -1,41 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { AiOutlineClockCircle, AiOutlineCalendar } from "react-icons/ai";
-import './Subcomplete_comp.css'
+import './Subcomplete_comp.css';
 
 function Subcomplete_comp() {
-    const [checked, setChecked] = React.useState(false);
+  const [isChecked, setIsChecked] = useState(false);
 
-  const handleChange = () => {
-    setChecked(!checked);
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
   };
-
 
   return (
     <div className='Subcomplete_comp'>
-        <div className='pending_img'>
-            <img src='https://avatars.githubusercontent.com/u/43471295?v=4' className='pending_img'/>
-        </div>
-        <div className='pending_body'>
-            <div className='pending_title'>
-                <h1>Smart Contracts</h1>
-                <h2 className='subject_heading'>Blockchain Technology</h2>
-            </div>  
-            <div className='pending_deadline'>
-                <div className='deadline_det'>
-                    <AiOutlineCalendar className='deadline_icon'/>
-                    <h1>12/05/2023</h1>
-                </div>
-                <div className='deadline_det'>
-                    <AiOutlineClockCircle className='deadline_icon' />
-                    <h1>9:00am</h1>
-                </div>
-            </div>
+      <label className='check'>
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+        />
+      </label>
+      <div className='pending_body'>
+        <div className='pending_title'>
+          <h2>Anjali Rajendran</h2>
          
+        </div>  
+        {
+        <div className='pending_deadline'>
+             <h3>Smart Contracts</h3>
+          <div className='deadline_det'>
+           
+            <AiOutlineCalendar className='deadline_icon'/>
+            <h3>Submitted on:</h3>
+            <h3>12/05/2023</h3>
+          </div>
+          <div className='deadline_det'>
+            <AiOutlineClockCircle className='deadline_icon' />
+            <h3>9:00am</h3>
+          </div>
         </div>
-        
-        
+        }
+      </div>
     </div>
-  )
+  );
 }
 
-export default Subcomplete_comp
+export default Subcomplete_comp;
--- src/pages/Subcomplete/Subcomplete.js
+++ src/pages/Subcomplete/Subcomplete.js
@@ -4,20 +4,19 @@ import './Subcomplete.css'
 
 function Subcomplete() {
   return (
-    <div className='Subcomplete'>
-        <div className='pending_head'>
-            <h1>Submitted Assignments</h1>
-        </div>
+   <div className='Subcomplete'><div className='pending_head'>
+    <h1>Submitted Assignments</h1>
+    
+           
         <div className='pending_cards'>
             <Subcomplete_comp />
             <Subcomplete_comp />
-            <Subcomplete_comp />
-            <Subcomplete_comp />
+           
         </div>
         <div className='check_button'>
-                <button className='plag_check_btn'>Check Plagiarism</button>
+                <button className='plag_check_btn'>Check for Plagiarism</button>
             </div>
-    </div>
+    </div></div> 
   )
 }
 
